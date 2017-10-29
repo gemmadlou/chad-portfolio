d73a4c239acfd4f20a78324d99f6dde358375069
Gemma Black <gblack@googlemail.com>
2017-10-29 03:38:37 +0000

    Fixed Issue With Gallery Pushing Page Out

--- app/src/Framework/Components/Image-Gallery/imagegallery.js
+++ app/src/Framework/Components/Image-Gallery/imagegallery.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 export default function () {
     return <div className="image-gallery">
-      <div className="image-gallery__item">
-        <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
-      </div>
-      <div className="image-gallery__item">
-        <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
-      </div>
-      <div className="image-gallery__item">
-        <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
-      </div>
-      <div className="image-gallery__item">
-        <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
-      </div>
-      <div className="image-gallery__item">
-        <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
-      </div>
-      <div className="image-gallery__item">
-        <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+      <div className="image-gallery__inner">
+        <div className="image-gallery__item">
+          <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+        </div>
+        <div className="image-gallery__item">
+          <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+        </div>
+        <div className="image-gallery__item">
+          <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+        </div>
+        <div className="image-gallery__item">
+          <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+        </div>
+        <div className="image-gallery__item">
+          <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+        </div>
+        <div className="image-gallery__item">
+          <img className="image-gallery__image" src="http://unsplash.it/800/800?image=888" />
+        </div>
       </div>
     </div>
 }
