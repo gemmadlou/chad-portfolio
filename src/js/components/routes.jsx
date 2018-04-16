4250fda4c2e73bb5d5de84c0113625845cfa3b67
Gemma Black <gblackuk@googlemail.com>
2018-04-16 07:38:05 +0100

    About page mocked

--- src/js/components/routes.jsx
+++ src/js/components/routes.jsx
@@ -4,6 +4,7 @@ import Route from 'route-parser';
 import {Project} from '../pages/project.jsx';
 import {Four0Four} from '../pages/404.jsx';
 import {Contact} from '../pages/contact.jsx';
+import {About} from '../pages/about.jsx';
 
 let routes = [
     {
@@ -13,6 +14,10 @@ let routes = [
     {
         router: new Route('contact'),
         component: <Contact />
+    },
+    {
+        router: new Route('about'),
+        component: <About />
     }
 ];
 
--- src/js/pages/contact.jsx
+++ src/js/pages/contact.jsx
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 
 export const Contact = () => {
     return <div className="h-100 overflow-y-auto roboto ">
-        <div className="fl-m h-50 relative w-60-m w-75-l h-100-m">
+        <div className="fl-m h-50 relative w-60-m w-two-thirds-l h-100-m">
             <img className="of-cover absolute top-0 left-0 h-100 w-100" src='http://unsplash.it/1000/1000' />
         </div>
-        <div className="lh-copy pa3 pa4-m pl5-l w-30-m w-25-l fl-m">
-            <h1>Get in contact</h1>
+        <div className="lh-copy f5 pa3 pa4-m pl5-l w-40-m w-third-l fl-m">
+            <h1 className="fw6 f3 mb4">Get in contact</h1>
 
-            <h3 className="fw6 f4">Email</h3>
+            <h3 className="fw6">Email</h3>
             <p className="mb4">hello@world.com</p>
 
-            <h3 className="fw6 f4">Phone</h3>
+            <h3 className="fw6">Phone</h3>
             <p className="mb4">01234 567 890</p>
 
-            <h3 className="fw6 f4">Address</h3>
+            <h3 className="fw6">Address</h3>
             <p className="mb4">44 canvas street
                 <br />Regan Road
                 <br />EC1 4AD
