b48eddea4437187022aa57c19104a6bbb92dcd12
Gemma Black <gemma.black@breakthroughmedia.org>
2018-04-16 13:47:34 +0100

    Started mocking project page

--- src/js/components/routes.jsx
+++ src/js/components/routes.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Route from 'route-parser';
-import {Project} from '../pages/project.jsx';
+import {Home} from '../pages/home.jsx';
 import {Four0Four} from '../pages/404.jsx';
 import {Contact} from '../pages/contact.jsx';
 import {About} from '../pages/about.jsx';
+import {Project} from '../pages/project.jsx';
 
 let routes = [
     {
         router: new Route(''),
-        component: <Project />
+        component: <Home />
     },
     {
         router: new Route('contact'),
@@ -18,6 +19,10 @@ let routes = [
     {
         router: new Route('about'),
         component: <About />
+    },
+    {
+        router: new Route('project/:project'),
+        component: <Project />
     }
 ];
 
--- src/js/pages/project.jsx
+++ src/js/pages/project.jsx
@@ -1,27 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {store} from '../infrastructure/state';
-
-const panels = store => {
-    let projects = store.getState().projects;
-    return projects.map((project, index) => {
-        return <li key={index} className="relative">
-            <img className="of-cover absolute top-0 left-0 h-100 w-100" src={project.image} />
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black-90"></div>
-            <div className="roboto white-90 sans-serif relative dt f3 w-100 h-100 ttu">
-                <div className="dtc tc v-mid">
-                    <div className="mb4 fw8 ls4">{project.client}</div>
-                    <button className="dim ls4 bg-black white bw0 fw8 ttu pl5 pr5 pt3 pb3 f6">Read</button>
-                </div>
-            </div>
-        </li>
-    });
-}
 
 export const Project = () => {
-    return <div className="h-100 overflow-y-auto">
+    return <div className="h-100 overflow-y-auto roboto">
+
+        <div className="h-100 relative">
+            <img className="of-cover absolute top-0 left-0 h-100 w-100" src='http://unsplash.it/1000/1000' />
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black-10"></div>
+            <div className=" white-90 sans-serif relative dt f3 w-100 h-100 ttu">
+                <div className="dtc tc v-mid">
+                    <span className="mb4 fw8 ls4">Puma</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="min-h-100 relative bg-light-gray">
+            <div className="fl-m h-50 relative left-0 top-0 absolute-m w-60-m w-two-thirds-l h-100-m">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src='http://unsplash.it/1000/1000' />
+            </div>
+            <div className="lh-copy fr-m f5 relative m-50-m pa3 pa4-m pl5-l pr5-l w-40-m w-third-l">
+                <h1>What we do</h1>
+
+                <p className="mb4">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                </p>
+                <p className="mb4">
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                </p>
+            </div>
+        </div>
+
+        <div className="h-100 relative">
+            <div className="black sans-serif relative dt f3 w-100 h-100 ttu">
+                <div className="dtc tc v-mid">
+                    <span className="mb4 fw8 ls4">Lorem ipsum dolor sit amet</span>
+                </div>
+            </div>
+        </div>
+
         <ul className="relative dg gac-1 gar-1 gac-2-ns gtc-2-ns gtr-2-ns gar-2-ns gtc-3-m gtr-2-m gar-2-m gac-3-m h-100 min-h-100-ns">
-            {panels(store)}
+            <li className="relative">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src="http://unsplash.it/1000/1000" />
+            </li>
+            <li className="relative">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src="http://unsplash.it/1000/1000" />
+            </li>
+            <li className="relative">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src="http://unsplash.it/1000/1000" />
+            </li>
+            <li className="relative">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src="http://unsplash.it/1000/1000" />
+            </li>
+            <li className="relative">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src="http://unsplash.it/1000/1000" />
+            </li>
+            <li className="relative">
+                <img className="of-cover absolute top-0 left-0 h-100 w-100" src="http://unsplash.it/1000/1000" />
+            </li>
         </ul>
-</div>;
+
+    </div>
 }
